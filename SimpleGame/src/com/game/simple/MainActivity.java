999c03a59b44b3b98da50227a2d3b549cfa5c596
dazziest <dazziest@gmail.com>
2014-03-06 14:35:20 +0800

    Add Physic World, add physic body on player

--- SimpleGame/src/com/game/simple/MainActivity.java
+++ SimpleGame/src/com/game/simple/MainActivity.java
@@ -4,12 +4,23 @@ import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.primitive.Rectangle;
+import org.andengine.entity.scene.IOnAreaTouchListener;
+import org.andengine.entity.scene.IOnSceneTouchListener;
+import org.andengine.entity.scene.ITouchArea;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.AutoParallaxBackground;
 import org.andengine.entity.scene.background.ParallaxBackground.ParallaxEntity;
 import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.entity.util.FPSLogger;
+import org.andengine.extension.physics.box2d.PhysicsConnector;
+import org.andengine.extension.physics.box2d.PhysicsFactory;
+import org.andengine.extension.physics.box2d.PhysicsWorld;
+import org.andengine.extension.physics.box2d.util.Vector2Pool;
+import org.andengine.input.sensor.acceleration.AccelerationData;
+import org.andengine.input.sensor.acceleration.IAccelerationListener;
+import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.texture.TextureOptions;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
@@ -18,8 +29,12 @@ import org.andengine.opengl.texture.region.TiledTextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
 
-import android.app.Activity;
-import android.os.Bundle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+
+import android.hardware.SensorManager;
 import android.view.Display;
 import android.view.WindowManager;
 
@@ -30,7 +45,7 @@ import android.view.WindowManager;
  * @author Nicolas Gramlich
  * @since 19:58:39 - 19.07.2010
  */
-public class MainActivity extends SimpleBaseGameActivity {
+public class MainActivity extends SimpleBaseGameActivity implements IAccelerationListener, IOnSceneTouchListener, IOnAreaTouchListener {
 	// ===========================================================
 	// Constants
 	// ===========================================================
@@ -44,13 +59,17 @@ public class MainActivity extends SimpleBaseGameActivity {
 
 	private BitmapTextureAtlas mBitmapTextureAtlas;
 	private TiledTextureRegion mPlayerTextureRegion;
-	private TiledTextureRegion mEnemyTextureRegion;
+//	private TiledTextureRegion mEnemyTextureRegion;
 
 	private BitmapTextureAtlas mAutoParallaxBackgroundTexture;
 
 	private ITextureRegion mParallaxLayerBack;
-//	private ITextureRegion mParallaxLayerMid;
-//	private ITextureRegion mParallaxLayerFront;
+	private ITextureRegion mParallaxLayerMid;
+	private ITextureRegion mParallaxLayerFront;
+	private PhysicsWorld mPhysicsWorld;
+	private float mGravityX;
+	private float mGravityY = 49f;
+	private AnimatedSprite player;
 
 	// ===========================================================
 	// Constructors
@@ -83,39 +102,71 @@ public class MainActivity extends SimpleBaseGameActivity {
 	public void onCreateResources() {
 		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
 		
-		this.mBitmapTextureAtlas = new BitmapTextureAtlas(this.getTextureManager(), 256, 128, TextureOptions.BILINEAR);
+		this.mBitmapTextureAtlas = new BitmapTextureAtlas(this.getTextureManager(), 1024, 1024, TextureOptions.BILINEAR);
 		this.mPlayerTextureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(this.mBitmapTextureAtlas, this, "player.png", 0, 0, 3, 4);
 //		this.mEnemyTextureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(this.mBitmapTextureAtlas, this, "enemy.png", 73, 0, 3, 4);
 		this.mBitmapTextureAtlas.load();
 
 		this.mAutoParallaxBackgroundTexture = new BitmapTextureAtlas(this.getTextureManager(), 1024, 1024);
-//		this.mParallaxLayerFront = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mAutoParallaxBackgroundTexture, this, "parallax_background_layer_front.png", 0, 0);
-		this.mParallaxLayerBack = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mAutoParallaxBackgroundTexture, this, "atlas.png", 0, 0);
-//		this.mParallaxLayerMid = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mAutoParallaxBackgroundTexture, this, "parallax_background_layer_mid.png", 0, 669);
+		this.mParallaxLayerFront = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mAutoParallaxBackgroundTexture, this, "parallax_background_layer_front.png", 0, 0);
+		this.mParallaxLayerBack = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mAutoParallaxBackgroundTexture, this, "parallax_background_layer_back.png", 0, 188);
+		this.mParallaxLayerMid = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mAutoParallaxBackgroundTexture, this, "parallax_background_layer_mid.png", 0, 669);
 		this.mAutoParallaxBackgroundTexture.load();
 	}
 
 	@Override
 	public Scene onCreateScene() {
 		this.mEngine.registerUpdateHandler(new FPSLogger());
-
+		
+		//create Physic world
+		this.mPhysicsWorld = new PhysicsWorld(new Vector2(0, SensorManager.GRAVITY_EARTH), false);
+		//create scene
 		final Scene scene = new Scene();
 		final AutoParallaxBackground autoParallaxBackground = new AutoParallaxBackground(0, 0, 0, 5);
 		final VertexBufferObjectManager vertexBufferObjectManager = this.getVertexBufferObjectManager();
 		autoParallaxBackground.attachParallaxEntity(new ParallaxEntity(0.0f, new Sprite(0, CAMERA_HEIGHT - this.mParallaxLayerBack.getHeight(), this.mParallaxLayerBack, vertexBufferObjectManager)));
-//		autoParallaxBackground.attachParallaxEntity(new ParallaxEntity(-5.0f, new Sprite(0, 80, this.mParallaxLayerMid, vertexBufferObjectManager)));
-//		autoParallaxBackground.attachParallaxEntity(new ParallaxEntity(-10.0f, new Sprite(0, CAMERA_HEIGHT - this.mParallaxLayerFront.getHeight(), this.mParallaxLayerFront, vertexBufferObjectManager)));
+		autoParallaxBackground.attachParallaxEntity(new ParallaxEntity(-5.0f, new Sprite(0, 80, this.mParallaxLayerMid, vertexBufferObjectManager)));
+		autoParallaxBackground.attachParallaxEntity(new ParallaxEntity(-10.0f, new Sprite(0, CAMERA_HEIGHT - this.mParallaxLayerFront.getHeight(), this.mParallaxLayerFront, vertexBufferObjectManager)));
 		scene.setBackground(autoParallaxBackground);
+		scene.setOnSceneTouchListener(this);
+		
+		//set rectangle boundary
+		final Rectangle ground = new Rectangle(0, CAMERA_HEIGHT - 2, CAMERA_WIDTH, 2, vertexBufferObjectManager);
+		final Rectangle roof = new Rectangle(0, 0, CAMERA_WIDTH, 2, vertexBufferObjectManager);
+		final Rectangle left = new Rectangle(0, 0, 2, CAMERA_HEIGHT, vertexBufferObjectManager);
+		final Rectangle right = new Rectangle(CAMERA_WIDTH - 2, 0, 2, CAMERA_HEIGHT, vertexBufferObjectManager);
+
+		//create wall
+		final FixtureDef wallFixtureDef = PhysicsFactory.createFixtureDef(0, 0.5f, 0.5f);
+		PhysicsFactory.createBoxBody(this.mPhysicsWorld, ground, BodyType.StaticBody, wallFixtureDef);
+		PhysicsFactory.createBoxBody(this.mPhysicsWorld, roof, BodyType.StaticBody, wallFixtureDef);
+		PhysicsFactory.createBoxBody(this.mPhysicsWorld, left, BodyType.StaticBody, wallFixtureDef);
+		PhysicsFactory.createBoxBody(this.mPhysicsWorld, right, BodyType.StaticBody, wallFixtureDef);
+
+		scene.attachChild(ground);
+		scene.attachChild(roof);
+		scene.attachChild(left);
+		scene.attachChild(right);
+
+		scene.registerUpdateHandler(this.mPhysicsWorld);
+
+		scene.setOnAreaTouchListener(this);
 
 		/* Calculate the coordinates for the face, so its centered on the camera. */
 		final float playerX = (CAMERA_WIDTH - this.mPlayerTextureRegion.getWidth()) / 2;
-		final float playerY = (CAMERA_HEIGHT - this.mPlayerTextureRegion.getHeight()) - 5;
+		final float playerY = (CAMERA_HEIGHT - this.mPlayerTextureRegion.getHeight()) / 2;
 
 		/* Create two sprits and add it to the scene. */
-		final AnimatedSprite player = new AnimatedSprite(playerX, playerY, this.mPlayerTextureRegion, vertexBufferObjectManager);
+		player = new AnimatedSprite(playerX, playerY, this.mPlayerTextureRegion, vertexBufferObjectManager);
+		final FixtureDef objectFixtureDef = PhysicsFactory.createFixtureDef(-50f, 0.0f, 100f);
+		final Body body = PhysicsFactory.createBoxBody(this.mPhysicsWorld, player, BodyType.DynamicBody, objectFixtureDef);
+		
+		this.mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(player, body, true, true));
 		player.setScaleCenterY(this.mPlayerTextureRegion.getHeight());
-		player.setScale(8);
+		player.setScale(2);
 		player.animate(new long[]{200, 200, 200}, 3, 5, true);
+		player.setUserData(body);
+//		scene.registerTouchArea(player);
 
 //		final AnimatedSprite enemy = new AnimatedSprite(playerX - 80, playerY, this.mEnemyTextureRegion, vertexBufferObjectManager);
 //		enemy.setScaleCenterY(this.mEnemyTextureRegion.getHeight());
@@ -124,8 +175,73 @@ public class MainActivity extends SimpleBaseGameActivity {
 
 		scene.attachChild(player);
 //		scene.attachChild(enemy);
+		
+//		final VertexBufferObjectManager vertexBufferObjectManager = this.getVertexBufferObjectManager();
+		
 
 		return scene;
+	}
+
+	@Override
+	public boolean onAreaTouched(TouchEvent pSceneTouchEvent, ITouchArea pTouchArea, float pTouchAreaLocalX, float pTouchAreaLocalY) {
+//		if(pSceneTouchEvent.isActionDown()) {
+//			final AnimatedSprite player = (AnimatedSprite) pTouchArea;
+//			this.jumpFace(player);
+//			return true;
+//		}
+		return false;
+	}
+	
+	private void jumpFace(final AnimatedSprite face) {
+		final Body faceBody = (Body)face.getUserData();
+
+//		int jumpX = -50;
+		int jumpY = -50;
+
+		final Vector2 velocity = Vector2Pool.obtain(this.mGravityX , this.mGravityY * jumpY);
+		faceBody.setLinearVelocity(velocity);
+		Vector2Pool.recycle(velocity);
+	}
+
+	@Override
+	public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
+		if(this.mPhysicsWorld != null) {
+			if(pSceneTouchEvent.isActionDown()) {
+//				final AnimatedSprite player = (AnimatedSprite) pTouchArea;
+				this.jumpFace(player);
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void onAccelerationAccuracyChanged(AccelerationData pAccelerationData) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onAccelerationChanged(AccelerationData pAccelerationData) {
+//		this.mGravityX = pAccelerationData.getX();
+////		this.mGravityY = pAccelerationData.getY();
+//
+//		final Vector2 gravity = Vector2Pool.obtain(this.mGravityX, this.mGravityY);
+//		this.mPhysicsWorld.setGravity(gravity);
+//		Vector2Pool.recycle(gravity);
+	}
+	
+	@Override
+	public void onResumeGame() {
+		super.onResumeGame();
+
+		this.enableAccelerationSensor(this);
+	}
+
+	@Override
+	public void onPauseGame() {
+		super.onPauseGame();
+
+		this.disableAccelerationSensor();
 	}
 
 	// ===========================================================
